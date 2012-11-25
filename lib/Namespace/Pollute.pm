9bbdd3d655afc9aca4b2e350434f35487cd6e585
sean <sean@blanton.com>
2012-11-25 16:44:17 -0600

    dzil

--- lib/Namespace/Pollute.pm
+++ lib/Namespace/Pollute.pm
@@ -1,4 +1,5 @@
 package Namespace::Pollute;
+# ABSTRACT: Loads and imports the default exported symbols of a list of modules
 
 use Exporter;
 @ISA = qw(Exporter);
@@ -43,3 +44,23 @@ sub import {
 }
 
 1;
+
+__END__
+
+=head1 SYNOPSIS
+
+  use Namspace::Pollute qw(-verbose My::Exporter1 My::Exporter2);
+
+=head1 DESCRIPTION
+
+Takes a list of modules, loads them, and exports their symbols into the current namespace. This first version provides only
+a small advantage in brevity over a list of 'use' statements, but has the ability to print out the symbols as they are exported
+for modules that inherit from 'Exporter'.
+
+For modules that don't inherit from 'Exporter', those modules' 'import' method will be called.
+
+=head1 TODO
+
+1) Make it work for Exporter's EXPORT_OK. 2) Export symbols to a variable number of levels.
+
+=cut
--- t/basic.t
+++ t/basic.t
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
-#
+use Test::More tests => 2;
 
 use Namespace::Pollute qw(-verbose common::sense Carp);
 
-say 'hi';
-carp 'ho';
+ok(say 'hi');
+ok(carp 'ho');
